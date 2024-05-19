b6e736b7a4bedc547eb69013b07b6676d149e434
shubham <shubhamdixit863@gmail.com>
2024-05-19 11:29:08 +0530

    modified e2e tests
    Signed-off-by: shubham <shubhamdixit863@gmail.com>

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+//console.log("hello world"); // print your name here 
+
+// Js variables --->
+// variables are used to store the data 
+// in js there are three ways to create a variable 
+// using var ,using let ,using const
+var num1=45;  // names should always start with an alphabet and they should be alpha numeric (abcd--z ,and 0-9)
+var num2=20;
+
+var sum=num1+num2;
+console.log(sum);
+
+let num3=78;
+let num4=90;
+
+const num45=89;
+
+
+// you have to calculate simple interest  ,si=p*r*t/100
+
+let principal=9000;
+let rate=10;
+let time=10;
+
+let si=(principal*rate*time)/100
+console.log(si);
